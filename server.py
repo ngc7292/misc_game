9ffb6de22786e3511f0a234275c0e65ab12e1f31
ngc <feizhaoye@gmail.com>
2018-11-08 16:42:10 +0800

    fix some bug

--- server.py
+++ server.py
@@ -28,6 +28,7 @@ import hashlib
 import requests
 import logging
 from logging import handlers
+import os
 
 class Logger(object):
     level_relations = {
@@ -40,13 +41,13 @@ class Logger(object):
 
     def __init__(self,filename,level='info',when='D',backCount=30,fmt='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'):
         self.logger = logging.getLogger(filename)
-        format_str = logging.Formatter(fmt)#设置日志格式
-        self.logger.setLevel(self.level_relations.get(level))#设置日志级别
-        sh = logging.StreamHandler()#往屏幕上输出
-        sh.setFormatter(format_str) #设置屏幕上显示的格式
+        format_str = logging.Formatter(fmt)
+        self.logger.setLevel(self.level_relations.get(level))
+        sh = logging.StreamHandler()
+        sh.setFormatter(format_str)
         th = handlers.TimedRotatingFileHandler(filename=filename,when=when,backupCount=backCount,encoding='utf-8')#往文件里写入#指定间隔时间自动生成文件的处理器
-        th.setFormatter(format_str)#设置文件里写入的格式
-        self.logger.addHandler(sh) #把对象加到logger里
+        th.setFormatter(format_str)
+        self.logger.addHandler(sh)
         self.logger.addHandler(th)
         
 access_log = Logger(filename="access.log")
@@ -54,26 +55,16 @@ error_log = Logger(filename="error.log",level='error')
 success_log = Logger(filename="success.log")
 
 START_TXT = """
+ _______  _______  _______  _______    _______  _______  _______  _______
+(  ____ \(  ____ )(  ____ \(  ___  )  (  ____ \(  ___  )(       )(  ____ \
+| (    \/| (    )|| (    \/| (   ) |  | (    \/| (   ) || () () || (    \/
+| (__    | (____)|| (__    | |   | |  | |      | (___) || || || || (__
+|  __)   |     __)|  __)   | |   | |  | | ____ |  ___  || |(_)| ||  __)
+| (      | (\ (   | (      | | /\| |  | | \_  )| (   ) || |   | || (
+| )      | ) \ \__| (____/\| (_\ \ |  | (___) || )   ( || )   ( || (____/\
+|/       |/   \__/(_______/(____\/_)  (_______)|/     \||/     \|(_______/
 
 
-
- _______ _________ _______  _______    _______  _______  _______  _______
-(       )\__   __/(  ____ \(  ____ \  (  ____ \(  ___  )(       )(  ____ \\
-| () () |   ) (   | (    \/| (    \/  | (    \/| (   ) || () () || (    \/
-| || || |   | |   | (_____ | |        | |      | (___) || || || || (__
-| |(_)| |   | |   (_____  )| |        | | ____ |  ___  || |(_)| ||  __)
-| |   | |   | |         ) || |        | | \_  )| (   ) || |   | || (
-| )   ( |___) (___/\____) || (____/\  | (___) || )   ( || )   ( || (____/\\
-|/     \|\_______/\_______)(_______/  (_______)|/     \||/     \|(_______/
-
-
-
-this is a sample eazy game and i will give you a lot of numbers and you can
-check them and get the flag, every level means some info of flag.
-
-hint is here!!!!
-
-input y to start this game, and input hint to get hint:
 """
 
 HINT_TXT = """
@@ -84,12 +75,16 @@ the data come form:
 
 #!/usr/bin/env python
 
+def get_number(x, freq,rge):
+    y = np.sin(2*np.pi*x*freq)*rge
+    return y
+    
 def divide_flag(token):
     flag_list = []
     flag = "********************************"
-    for i in range(0,32,2):
+    for i in range(0,64,2):
         flag_list.append(int(flag[i]+flag[i+1],16))
-    return flag_list
+    return flag,flag_list
     
 def game(level,flag_list):
     
@@ -104,23 +99,41 @@ def game(level,flag_list):
         else:
             y += get_number(x,freq,7)
     return y,freq_list
+    
+def start_game(tcpClisock,token,userlogger):
+    
+    flag,flag_list = divide_flag(token)
+    level = 0
+    while True:
+        if level == 8:
+            tcpClisock.sendall((CONGRATULATION_TXT.format("hctf{"+flag+"}")).encode("utf-8"))
+            break
+        y,freq_list = game(level,flag_list)
+        send_data = json.dumps(list(y)).encode("utf-8")
+        tcpClisock.sendall(send_data)
+        req_data = tcpClisock.recv(1024).decode("utf-8").replace("\n","")
+        req = req_data.split(" ")
+        req.sort()
+        freq_list.sort()
+        if req == freq_list:
+            level += 1
+            continue
+        else:
+            break
+    tcpClisock.close()
 
 """
 
 CONGRATULATION_TXT = """
 
-congratulations ,you solve the whole game and flag is :
-
-hctf{ ''.join([hex(freq) for freq in every freq you get]) }
-
-totle length of whole flag whithout hctf{} is 32
+congratulations ,you solve the whole game and flag is :{0}
 
 """
 
 flag_list = []
 
 def server():
-    host = "127.0.0.1"
+    host = "0.0.0.0"
     port = 6775
     BUFFSIZE = 1024
     
@@ -140,31 +153,40 @@ def server():
         p.start()
         
 def problem(tcpClisock,cli_addr):
+    file_name = str(cli_addr[0])+".log"
+    userlogger = Logger(filename=file_name)
+    
     tcpClisock.sendall(START_TXT.encode("utf-8"))
-    data = tcpClisock.recv(1024)
-    data = data.decode("utf-8").replace("\n","")
-    if data == "hint":
-        tcpClisock.sendall(HINT_TXT.encode("utf-8"))
-        tcpClisock.close()
-    elif data == "y":
-        tcpClisock.sendall("input your token:".encode("utf-8"))
-        token = tcpClisock.recv(1024).decode("utf-8").replace("\n","")
-        if check_token(token) == True:
-            access_log.logger.info("token "+str(token)+" access in")
-            start_game(tcpClisock,token)
+    while True:
+        tcpClisock.sendall("input y to start this game, and input hint to get hint:".encode("utf-8"))
+        data = tcpClisock.recv(1024)
+        data = data.decode("utf-8").replace("\n","")
+        userlogger.logger.info("user input: "+str(data))
+        if data == "hint":
+            tcpClisock.sendall(HINT_TXT.encode("utf-8"))
+        elif data == "y":
+            tcpClisock.sendall("input your token:".encode("utf-8"))
+            token = tcpClisock.recv(1024).decode("utf-8").replace("\n","")
+            if check_token(token) == True:
+                userlogger.logger.info("token "+str(token)+" access in")
+                start_game(tcpClisock,token,userlogger)
+                return
+            else:
+                tcpClisock.sendall("your token is error".encode("utf-8"))
+                tcpClisock.shutdown(2)
+                break
         else:
-            tcpClisock.sendall("your token is error".encode("utf-8"))
-            tcpClisock.close()
-    else:
-        tcpClisock.close()
+            tcpClisock.sendall("your input is error".encode("utf-8"))
+            tcpClisock.shutdown(2)
+            break
         
 def divide_flag(token):
     flag_list = []
     flag_salt = "fFt_Is_5o_Ea2y"
-    flag = hashlib.md5(str(token+flag_salt).encode("utf-8")).hexdigest()
-    for i in range(0,32,2):
+    flag = hashlib.sha256(str(token+flag_salt).encode("utf-8")).hexdigest()
+    for i in range(0,64,2):
         flag_list.append(int(flag[i]+flag[i+1],16))
-    return flag_list
+    return flag,flag_list
 
 def check_token(token):
     try:
@@ -180,20 +202,23 @@ def check_token(token):
 def get_verify():
     pass
 
-def start_game(tcpClisock,token):
+def start_game(tcpClisock,token,userlogger):
     
-    flag_list = divide_flag(token)
-    
+    flag,flag_list = divide_flag(token)
     level = 0
     while True:
-        if level == 4:
-            tcpClisock.sendall(CONGRATULATION_TXT.encode("utf-8"))
-            success_log.logger.info(str(token)+" get the flag")
+        if level == 8:
+            tcpClisock.sendall((CONGRATULATION_TXT.format("hctf{"+flag+"}")).encode("utf-8"))
+            
+            userlogger.logger.info(str(token)+" get the flag")
+            
             break
         y,freq_list = game(level,flag_list)
         send_data = json.dumps(list(y)).encode("utf-8")
         tcpClisock.sendall(send_data)
         req_data = tcpClisock.recv(1024).decode("utf-8").replace("\n","")
+        userlogger.logger.info("user recv: "+str(req_data))
+        userlogger.logger.info("success freq: "+str(freq_list))
         req = req_data.split(" ")
         req.sort()
         freq_list.sort()
@@ -202,7 +227,7 @@ def start_game(tcpClisock,token):
             continue
         else:
             break
-    tcpClisock.close()
+    tcpClisock.shutdown(2)
         
 
 def game(level,flag_list):
--- solution.py
+++ solution.py
@@ -24,60 +24,90 @@ import json
 import numpy as np
 from scipy.fftpack import fft
 import socket
+from multiprocessing import Process
+import math
 
-token = "rf5LAktSVS0XnOEyC8Z4JoFKQf8X9yln"
+token = "Okxa47uIRWgnQCdtAUIQMBbowEZFOSIb"
 
-x = np.linspace(0,1,1500)
-
-def get_freq(x,y):
-    yy = fft(y)
-    y_real = yy.real
-    y_imag = yy.imag
-
-    yy = abs(fft(y))
-    xx = range(len(y))
-
-    yf = abs(fft(y))  # 取绝对值
-    yf1 = abs(fft(y)) / len(x)  # 归一化处理
-    yf2 = yf1[range(int(len(x) / 2))]  # 由于对称性，只取一半区间
-
-    xf = np.arange(len(y))  # 频率
-    xf1 = xf
-    xf2 = xf[range(int(len(x) / 2))]  # 取一半区间
-
-    res = []
-    for i, j in zip(xf2, yf2):
-        if int(j) != 0:
-            res.append(str(i))
-            
-    return " ".join(res)
-
-
-tcpCliSock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-tcpCliSock.connect(("127.0.0.1",6775))
-
-
-tcpCliSock.recv(1024)
-tcpCliSock.sendall("y".encode("utf-8"))
-tcpCliSock.recv(1024)
-tcpCliSock.sendall(token.encode("utf-8"))
-
-flag_list = []
-while True:
-    data = tcpCliSock.recv(65536)
-    if "con" in data.decode("utf-8"):
-        break
-    print(data)
-    data = json.loads(data.decode("utf-8"))
-    y = np.array(data)
-    res = get_freq(x,y)
-    print(res)
-    flag_list += res.split(" ")
-    tcpCliSock.sendall(res.encode("utf-8"))
-
-flag = ""
-for i in flag_list:
-    flag += hex(int(i))[2:].zfill(2)
-
-print("hctf{"+flag+"}")
+def solution():
+    x = np.linspace(0,1,1500)
     
+    def get_freq(x,y):
+        yy = fft(y)
+        y_real = yy.real
+        y_imag = yy.imag
+    
+        yy = abs(fft(y))
+        xx = range(len(y))
+    
+        yf = abs(fft(y))  # 取绝对值
+        yf1 = abs(fft(y)) / len(x)  # 归一化处理
+        yf2 = yf1[range(int(len(x) / 2))]  # 由于对称性，只取一半区间
+    
+        xf = np.arange(len(y))  # 频率
+        xf1 = xf
+        xf2 = xf[range(int(len(x) / 2))]  # 取一半区间
+    
+        res = []
+        res_y = []
+        for i, j in zip(xf2, yf2):
+            if int(j) != 0:
+                a = math.ceil(yf2[i]*2)
+                while a >= 7:
+                    res.append(str(i))
+                    res_y.append(str(a))
+                    a = a//2
+                
+        return res,res_y
+    
+    
+    tcpCliSock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    tcpCliSock.connect(("127.0.0.1",6775))
+    
+    
+    tcpCliSock.recv(1024)
+    tcpCliSock.sendall("y".encode("utf-8"))
+    tcpCliSock.recv(1024)
+    tcpCliSock.sendall(token.encode("utf-8"))
+    
+    flag_list = []
+    
+    
+    while True:
+        number_data = ""
+        while True:
+            data = tcpCliSock.recv(65536)
+            if data == b'':
+                break
+            print(data)
+            if "con" in data.decode("utf-8"):
+                break
+            elif "[" in data.decode("utf-8") and "]" in data.decode("utf-8"):
+                number_data = data.decode("utf-8")
+                break
+            elif "[" in data.decode("utf-8"):
+                number_data = data.decode(("utf-8"))
+            elif "]" in data.decode("utf-8"):
+                number_data += data.decode("utf-8")
+                break
+            else:
+                number_data += data.decode("utf-8")
+                
+        if "con" in data.decode("utf-8"):
+            break
+        data = number_data
+        print(data)
+        # print(len(data))
+        # print(data)
+        data = json.loads(data)
+        y = np.array(data)
+        res, res_y  = get_freq(x,y)
+        while len(res) < 4:
+            res = ["0"] + res
+        res = " ".join(res)
+        print(res, res_y)
+        flag_list += res.split(" ")
+        tcpCliSock.sendall(res.encode("utf-8"))
+
+if __name__ == '__main__':
+    solution()
